56cad55a989929a86d2834bde686ab3bd208c128
Stephen Vizzusi <svizzusi13@gmail.com>
2023-06-09 23:48:21 -0700

    First commit

--- server.js
+++ server.js
@@ -27,11 +27,13 @@ app.get('/', (request, response)=>{
     response.sendFile(__dirname + '/index.html')
 })
 
+
 app.get('/api/:rapperName', (request, response)=>{
     const rappersName = request.params.rapperName.toLowerCase();
     if(rappers[rappersName])
     response.json(rappers[rappersName])
 })
+
 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log('server running better catch it')
